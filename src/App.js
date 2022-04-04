6350bd05ec8a7c58c39f673bc496bdf432e7653b
Nursen-Okenov <you@example.com>
2022-04-04 14:09:36 +0300

    changed some codes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 const api = {
   key: "1cd2d4805d877726f21299c4352791e5",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
   const search = evt => {
@@ -14,38 +14,36 @@ function App() {
         .then(res => res.json())
         .then(result => {
           setWeather(result);
-          setQuery("");
+          setQuery('');
           console.log(result);
         });
     }
   }
 
-
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"];
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     let day = days[d.getDay()];
-    let date = d.getDay();
-    let month = months[d.getMonth()]
+    let date = d.getDate();
+    let month = months[d.getMonth()];
     let year = d.getFullYear();
 
     return `${day} ${date} ${month} ${year}`
-
   }
+
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? "appwarm" : "app") : "app"}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'appwarm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input
             type="text"
             className="input"
-            placeholder="Search"
+            placeholder="Search..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
-          ></input>
+          />
         </div>
         {(typeof weather.main != "undefined") ? (
           <div>
@@ -60,9 +58,8 @@ function App() {
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
-        ) : ("")}
+        ) : ('')}
       </main>
-
     </div>
   );
 }
